5cab431841e1ff9fe90eed49bb7c3bfb80d53851
f4bian-pacheco <fabian.paxe@gmail.com>
2023-04-07 23:05:32 -0400

    feat: calcular_distancia() y centroide mas cercano

--- programa2.c
+++ programa2.c
@@ -9,10 +9,12 @@
 
 typedef struct{
     float* data;
-} Vector;
+} Vector; // estructura para guardar los puntos y hacer mas simple la vista
 
 void mostrar_data(Vector* ELEMS, int N_DB, int dim);
 Vector* inicializar_centroides(Vector* ELEMS, int N_DB, int dim);
+float calcular_distancia(float* punto_1, float* centroide, int dim);
+int centroide_mas_cercano(float* punto, int dim_pto, Vector* centroides, int num_centroides);
 
 
 int main(){
@@ -42,14 +44,29 @@ int main(){
     Vector* centroides = inicializar_centroides(ELEMS, N_DB, DIM);
     
     mostrar_data(ELEMS, N_DB, DIM);
-    printf("centroides\n");
-    for (int i = 0; i<K; i++){
-        for (int j= 0; j < DIM; j++) {
-            float* temp_data = centroides[i].data;
+    printf("\ncentroides\n\n");
+    mostrar_data(centroides,K,DIM);
+    printf("%.2f\n", calcular_distancia(&ELEMS->data[0], &centroides->data[0] , DIM));
+    
+    int* idx_centroides_por_punto = (int*)malloc(N_DB*sizeof(int));
+
+    for (int i = 0; i < N_DB; i++) {
+        float* punto = ELEMS[i].data;
+        int indice_centroide_cercano = centroide_mas_cercano(punto, DIM, centroides, K);
+        idx_centroides_por_punto[i] = indice_centroide_cercano; 
+    }
+
+    printf("Centroides mas cercanos para cada punto:\n");
+    for (int i = 0; i < N_DB; i++) {
+        printf("%d -> ",idx_centroides_por_punto[i]);
+        for (int j = 0; j < DIM; j++) {
+            float* temp_data = ELEMS[i].data;
             printf("%.2f ", temp_data[j]); 
         }
         printf("\n");
+    
     }
+
     return 0;
 }
 
@@ -63,7 +80,6 @@ Vector* inicializar_centroides(Vector* ELEMS, int N_DB, int dim){
 
     for (int i = 0; i < K; i++) {
         centroide_idx = rand() % N_DB;
-        printf("%d\n", centroide_idx);
         centroide_elegido = ELEMS[centroide_idx];
         centroides[i] = centroide_elegido;
     }
@@ -75,13 +91,9 @@ Vector* inicializar_centroides(Vector* ELEMS, int N_DB, int dim){
 void mostrar_data(Vector* ELEMS, int N_DB, int dim){
     for (int i = 0; i < N_DB; i++) {
         float* temp_data = ELEMS[i].data;
+        printf("%d -> ",i);
         for (int j = 0; j < dim; j++) {
-            printf("%.2f",temp_data[j]);
-            if(j!=dim-1){
-                printf("-");
-            }else{
-                printf("\n");
-            }
+            printf("%.2f ",temp_data[j]);
         }
         printf("\n");
     }
@@ -89,4 +101,26 @@ void mostrar_data(Vector* ELEMS, int N_DB, int dim){
 }
 
 
+float calcular_distancia(float* punto_1, float* centroide, int dim){
+    float distancia = 0;
+    float diff = 0;
+    for (int i = 0; i < dim; i++) {
+        diff += powf(punto_1[i]-centroide[i], 2.0);
+    }
+    distancia = sqrtf(diff);
+    return distancia;
+}
 
+
+int centroide_mas_cercano(float* punto, int dim_pto, Vector* centroides, int num_centroides){
+    int idx_centroide_mas_cercano = 0;
+    float minima_distancia = INFINITY;
+    for(int i = 0; i < num_centroides; i++){
+        float distancia_temp = calcular_distancia(punto, centroides[i].data, dim_pto);
+        if (distancia_temp < minima_distancia) {
+            minima_distancia = distancia_temp;
+            idx_centroide_mas_cercano = i;
+        }
+    }
+    return idx_centroide_mas_cercano;
+}
