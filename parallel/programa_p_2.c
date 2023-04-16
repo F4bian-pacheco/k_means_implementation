f018ca9df973fa2528680443c1d8d6d1028c74c6
f4bian-pacheco <fabian.paxe@gmail.com>
2023-04-16 15:46:45 -0400

    feat: primera version paralela, faltan tests

--- parallel/programa_p_2.c
+++ parallel/programa_p_2.c
@@ -8,26 +8,10 @@
 #include <unistd.h>
 #include <omp.h>
 
-/*
-En este código, se implementó el algoritmo de clustering k-means.
-El programa mostraba resultados inesperados en la función de recalculo de centroides,
-donde se encontraron cambios no intencionales en el array ELEMS, que debería ser de solo lectura.
 
-Después de revisar el código y hacer algunas pruebas, se descubrió que el problema estaba en la función de inicialización de centroides.
-Al asignar el valor del vector aleatorio a la variable centroide_elegido, se estaba asignando su dirección de memoria en lugar de copiar el valor del vector.
-Entonces, al cambiar el valor de centroide_elegido, también se cambiaba el valor del vector correspondiente en ELEMS.
-
-La solución fue copiar el valor del vector en lugar de asignar la dirección de memoria, usando la función memcpy.
-Después de hacer este cambio, el programa funcionó correctamente y se evitó la corrupción no intencional de los datos en ELEMS.
-*/
-
-// TODO
-//* medir el tiempo
-
-#define DIM 2
-#define T 4
-#define K 2
-#define MAX_ITER 10
+#define DIM 20
+#define T 8
+#define K 4
 
 typedef struct
 {
@@ -36,10 +20,8 @@ typedef struct
 
 void mostrar_data(Vector *ELEMS, int N_DB, int dim);
 Vector *inicializar_centroides(Vector *ELEMS, int N_DB, int dim);
-Vector *inicializar_centroides_kmeanspp(Vector *ELEMS, int N_DB, int dim); // mas aleatorio
 
 float calcular_distancia(float *punto_1, float *centroide, int dim);
-double distancia_cuadrado(Vector x, Vector y, int dim); // esta se usa en el k-meanspp
 
 int centroide_mas_cercano(float *punto, int dim_pto, Vector *centroides, int num_centroides);
 void recalcular_centroides(Vector *centroides, int *idx_centroides_por_punto, Vector *ELEMS, int num_centroides, int num_puntos, int dim);
@@ -54,8 +36,6 @@ int main()
     Vector *ELEMS, *centroides;
     struct timeval t1, t2;
     float real_time;
-
-    int converge = 0;
 
     omp_set_num_threads(T);
 
@@ -84,9 +64,8 @@ int main()
     gettimeofday(&t1, 0);
     #pragma omp single
     {
-        centroides = inicializar_centroides_kmeanspp(ELEMS, N_DB, DIM);
+        centroides = inicializar_centroides(ELEMS, N_DB, DIM);
     }
-    // centroides = inicializar_centroides(ELEMS, N_DB, DIM);
 
     Vector *centroides_anteriores = (Vector *)malloc(K * sizeof(Vector));
     // copiando los valores de los centroides a los centroides anteriores
@@ -98,6 +77,33 @@ int main()
         centroides_anteriores[i] = v;
     }
 
+    int* idx_centroides_por_punto = (int*)calloc(N_DB,sizeof(int));// esto puede ser una funcion para mostrar lo que tengo que mostrar en pantalla
+    while(1){
+        #pragma omp parallel shared(N_DB, ELEMS, centroides, idx_centroides_por_punto)
+        {
+            int tid = omp_get_thread_num();
+            
+            for (int i = tid; i < N_DB; i += T) {
+                float* punto = ELEMS[i].data;
+                int indice_centroide_cercano = centroide_mas_cercano(punto, DIM, centroides, K);
+                idx_centroides_por_punto[i] = indice_centroide_cercano; 
+            }
+
+        }
+        recalcular_centroides(centroides, idx_centroides_por_punto, ELEMS, K, N_DB, DIM);
+        if (comparar_centroides(centroides, centroides_anteriores, K, DIM) == 1){
+            //mostrar_indices(idx_centroides_por_punto, N_DB);
+            break;
+        }
+        
+        for (int i = 0; i < K; i++) {
+            memcpy(centroides_anteriores[i].data, centroides[i].data, DIM*sizeof(float));
+        }
+        k++;
+    }
+
+    //mostrar_indices(idx_centroides_por_punto, N_DB);
+    printf("centroides finales\n");
     mostrar_data(centroides, K, DIM);
 
 
@@ -108,58 +114,6 @@ int main()
     printf("\nTiempo Real = %f\n", real_time);
 
     return 0;
-}
-
-Vector *inicializar_centroides_kmeanspp(Vector *ELEMS, int N_DB, int dim)
-{
-    Vector *centroides = (Vector *)malloc(K * sizeof(Vector));
-    srand(time(NULL));
-
-    // Seleccionar el primer centroide al azar
-    int centroide_idx = rand() % N_DB;
-    centroides[0] = ELEMS[centroide_idx];
-
-    // Seleccionar los siguientes centroides usando k-means++
-    for (int i = 1; i < K; i++)
-    {
-        // Calcular las distancias al cuadrado de cada punto al centroide más cercano
-        double *distancias = (double *)malloc(N_DB * sizeof(double));
-        for (int j = 0; j < N_DB; j++)
-        {
-            double min_dist = INFINITY;
-            for (int l = 0; l < i; l++)
-            {
-                double dist = distancia_cuadrado(ELEMS[j], centroides[l], dim);
-                if (dist < min_dist)
-                {
-                    min_dist = dist;
-                }
-            }
-            distancias[j] = min_dist;
-        }
-
-        // Seleccionar el siguiente centroide usando las distancias al cuadrado
-        double dist_total = 0.0;
-        for (int j = 0; j < N_DB; j++)
-        {
-            dist_total += distancias[j];
-        }
-        double r = ((double)rand() / RAND_MAX) * dist_total;
-        double acumulador = 0.0;
-        for (int j = 0; j < N_DB; j++)
-        {
-            acumulador += distancias[j];
-            if (acumulador >= r)
-            {
-                centroides[i] = ELEMS[j];
-                break;
-            }
-        }
-
-        free(distancias);
-    }
-
-    return centroides;
 }
 
 Vector *inicializar_centroides(Vector *ELEMS, int N_DB, int dim)
@@ -214,18 +168,10 @@ float calcular_distancia(float *punto_1, float *centroide, int dim)
     return distancia;
 }
 
-double distancia_cuadrado(Vector x, Vector y, int dim)
-{
-    double dist = 0.0;
-    for (int i = 0; i < dim; i++)
-    {
-        dist += pow(x.data[i] - y.data[i], 2);
-    }
-    return dist;
-}
-
 int centroide_mas_cercano(float *punto, int dim_pto, Vector *centroides, int num_centroides)
 {
+    // separar por hilos y guardar el centroide mas cercano elegido por cada hilo,
+    //  
     int idx_centroide_mas_cercano = 0;
     float minima_distancia = INFINITY;
     for (int i = 0; i < num_centroides; i++)
@@ -243,25 +189,26 @@ int centroide_mas_cercano(float *punto, int dim_pto, Vector *centroides, int num
 void recalcular_centroides(Vector *centroides, int *idx_centroides_por_punto, Vector *ELEMS, int num_centroides, int num_puntos, int dim)
 {
     // encontrar los puntos que pertenecen a cada centroide y calcular la media de sus coordenadas(esta media sera el nuevo centroide)
+    #pragma omp parallel for
     for (int i = 0; i < num_centroides; i++)
     {
         float *sum_puntos = (float *)calloc(dim, sizeof(float));
         // memset(centroides[i].data, 0, dim*sizeof(float));
 
         int count = 0; // cantidad de puntos que pertenecen al centroide i
-        for (int j = 0; j < num_puntos; j++)
-        {
-            if (idx_centroides_por_punto[j] == i)
-            {
-                float *punto_temp = ELEMS[j].data;
-                for (int l = 0; l < dim; l++)
-                {
+        
+        #pragma omp parallel for reduction(+:count)
+        for (int j = 0; j < num_puntos; j ++) {
+            if (idx_centroides_por_punto[j] == i) {
+                float* punto_temp = ELEMS[j].data;
+                for (int l = 0; l < dim; l++) {
                     sum_puntos[l] += punto_temp[l]; // sumo cada coordenada de todos los puntos
                     // centroides[i].data[l] += punto_temp[l]; // sumo cada coordenada de todos los puntos
                 }
                 count++;
             }
         }
+
         if (count != 0)
         {
             for (int j = 0; j < dim; j++)
